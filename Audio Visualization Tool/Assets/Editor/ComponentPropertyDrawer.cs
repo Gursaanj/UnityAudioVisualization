c4226a89551ac512db2ec4df9c671525ccc5e2e6
Gursaanj <gursaanjresides@gmail.com>
2020-05-01 18:56:48 -0700

    Added Custom GetComponent PropertyDrawer Scripts

---
+++ Audio Visualization Tool/Assets/Editor/ComponentPropertyDrawer.cs
@@ -0,0 +1,92 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Component), true)]
+public class ComponentPropertyDrawer : ObjectPropertyViewer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Component component = property.serializedObject.targetObject as Component;
+        if (component == null || property.isArray)
+        {
+            base.OnGUI(position, property, label);
+            return;
+        }
+
+        position.width -= WIDTH_OF_BUTTON * 3;
+        base.OnGUI(position, property, label);
+        position.x = position.xMax;
+        position.width = WIDTH_OF_BUTTON;
+
+        if (GUI.Button(position, new GUIContent("G", "Assign value based on GetComponent on this GameObject")))
+        {
+            System.Type type = GetPropertyType(property);
+
+            if (type != null)
+            {
+                HandlePropertyButtonClicked(property, component.GetComponents(type));
+            }
+            else
+            {
+                property.objectReferenceValue = null;
+            }
+        }
+
+        position.x += WIDTH_OF_BUTTON;
+        if (GUI.Button(position, new GUIContent("P", "Assign value based on GetCompent on the parent of this GameObject")))
+        {
+            System.Type type = GetPropertyType(property);
+            if (type != null)
+            {
+                HandlePropertyButtonClicked(property, component.GetComponentsInParent(type), component.gameObject);
+            }
+            else
+            {
+                property.objectReferenceValue = null;
+            }
+        }
+
+        position.x += WIDTH_OF_BUTTON;
+        if (GUI.Button(position, new GUIContent("C", "Assign value based on GetCompent on the children of this GameObject")))
+        {
+            System.Type type = GetPropertyType(property);
+            if (type != null)
+            {
+                HandlePropertyButtonClicked(property, component.GetComponentsInChildren(type), component.gameObject);
+            }
+            else
+            {
+                property.objectReferenceValue = null;
+            }
+        }
+    }
+
+    private void HandlePropertyButtonClicked(SerializedProperty property, Component[] components, GameObject exclude = null)
+    {
+        GenericMenu menu = new GenericMenu();
+        Component defaultCompnent = null;
+        foreach (Component component in components)
+        {
+            if (component.gameObject == exclude)
+            {
+                continue;
+            }
+
+            defaultCompnent = component;
+            menu.AddItem(new GUIContent(string.Format("{0} ~ {1}", component.name, component.GetType().Name)), false, delegate ()
+            {
+                property.objectReferenceValue = component;
+                property.serializedObject.ApplyModifiedProperties();
+            });
+        }
+
+        if (menu.GetItemCount() > 1)
+        {
+            menu.ShowAsContext();
+        }
+        else
+        {
+            property.objectReferenceValue = defaultCompnent;
+        }
+    }
+}
---
+++ Audio Visualization Tool/Assets/Editor/ObjectPropertyViewer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(UnityEngine.Object), true)]
+public class ObjectPropertyViewer : PropertyDrawer
+{
+    protected const float WIDTH_OF_BUTTON = 20f;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        position.width -= WIDTH_OF_BUTTON;
+        EditorGUI.PropertyField(position, property, label, false);
+        position.x = position.xMax;
+        position.width = WIDTH_OF_BUTTON;
+
+        if (GUI.Button(position, new GUIContent("X", "Sets value to null")))
+        {
+            property.objectReferenceValue = null;
+        }
+    }
+
+    protected Type GetPropertyType(SerializedProperty prop)
+    {
+        string path = prop.propertyPath.Replace(".Array.data[", "[");
+        object obj = prop.serializedObject.targetObject;
+        string[] stringElements = path.Split('.');
+        if (stringElements.Length == 0)
+        {
+            return null;
+        }
+
+        for (int i = 0, len = stringElements.Length - 1; i < len; i++)
+        {
+            string element = stringElements[i];
+            int indexOf = element.IndexOf("[");
+            if (indexOf >= 0)
+            {
+                string stringElementName = element.Substring(0, indexOf);
+                int index = Convert.ToInt32(element.Substring(indexOf).Replace("[", string.Empty).Replace("]", string.Empty));
+                obj = GetValue(obj, stringElementName, index);
+            }
+            else
+            {
+                obj = GetValue(obj, element);
+            }
+        }
+
+        return GetTypeOfField(obj, stringElements[stringElements.Length - 1]);
+
+    }
+
+
+    protected Type GetTypeOfField(object sourceObject, string name)
+    {
+        if (sourceObject == null)
+        {
+            return null;
+        }
+
+        Type type = sourceObject.GetType();
+        FieldInfo info = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+        if (info == null)
+        {
+            PropertyInfo pInfo = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            if (pInfo == null)
+            {
+                return null;
+            }
+            return pInfo.PropertyType;
+        }
+
+        return info.FieldType;
+    }
+
+    protected object GetValue(object sourceObject, string name)
+    {
+        if (sourceObject == null)
+        {
+            return null;
+        }
+
+        Type type = sourceObject.GetType();
+        FieldInfo info = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+        if (info == null)
+        {
+            PropertyInfo pInfo = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            if (pInfo == null)
+            {
+                return null;
+            }
+            return pInfo.GetValue(sourceObject, null);
+        }
+
+        return info.GetValue(sourceObject);
+    }
+
+    protected object GetValue(object sourceObject, string name, int index)
+    {
+        IEnumerable enumerable = GetValue(sourceObject, name) as IEnumerable;
+        IEnumerator enumerator = enumerable.GetEnumerator();
+
+        while (index-- >= 0)
+        {
+            if (!enumerator.MoveNext())
+            {
+                return null;
+            }
+        }
+
+        return enumerator.Current;
+    }
+}
