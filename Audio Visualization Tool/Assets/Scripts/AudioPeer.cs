31c42c32c9a07a3733d8b5bb4785338dd5a25a6c
Gursaanj <gursaanjresides@gmail.com>
2020-05-01 18:53:42 -0700

    Working 512 Sample Visualizer

--- Audio Visualization Tool/Assets/Scripts/AudioPeer.cs
+++ Audio Visualization Tool/Assets/Scripts/AudioPeer.cs
@@ -6,9 +6,9 @@ public class AudioPeer : MonoBehaviour
     // samples follow Frequency intervals, _samples[0]
     // will be the deepbase at 20hz Min
     // and _samples[512] will be highest pitch at 20 khz max
-    [SerializeField] private float[] _samples = new float[512];
+    public static float[] Samples = new float[512];
 
-    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSource = null;
 
     private void Update()
     {
@@ -19,6 +19,6 @@ public class AudioPeer : MonoBehaviour
     {
         /// Window is the Fourier Alogrithm to reduce leakage
         /// I.e. smaller O() with more complex FFT Window - but reduced speed for method
-        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+        _audioSource.GetSpectrumData(Samples, 0, FFTWindow.Blackman);
     }
 }
---
+++ Audio Visualization Tool/Assets/Scripts/InstantiateCubes.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+    [SerializeField] private GameObject _sampleCubePrefab =  null;
+
+    [Range(1000, 100000)]
+    [SerializeField] private float _maxScale = 1000f;
+
+    private GameObject[] _sampleCubes = new GameObject[512];
+
+    private const string CUBE_NAME = "SampleCube";
+    private const float ANGLE_OF_SAMPLE_IN_CIRCLE = 360f / 512f;
+    private const float DISTANCE_BETWEEN_CUBES = 100f;
+    private const float CUBE_SCALE = 10f;
+
+
+    private void Start()
+    {
+        for (int i = 0; i < _sampleCubes.Length; i++)
+        {
+            GameObject instantiatedCube = (GameObject)Instantiate(_sampleCubePrefab);
+            instantiatedCube.transform.position = this.transform.position;
+            instantiatedCube.transform.parent = this.transform;
+            instantiatedCube.name = string.Format("{0}{1}", CUBE_NAME, i);
+
+            this.transform.eulerAngles = new Vector3(0, -i * ANGLE_OF_SAMPLE_IN_CIRCLE, 0);
+            instantiatedCube.transform.position = Vector3.forward * DISTANCE_BETWEEN_CUBES;
+
+            _sampleCubes[i] = instantiatedCube;
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < _sampleCubes.Length; i++)
+        {
+            if (_sampleCubes[i] != null)
+            {
+                _sampleCubes[i].transform.localScale = new Vector3(CUBE_SCALE, AudioPeer.Samples[i] * _maxScale, CUBE_SCALE);
+            }
+        }
+    }
+
+}
