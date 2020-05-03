888182145c2514696fccd95fa08b5d4147a06b1e
Gursaanj <gursaanjresides@gmail.com>
2020-05-03 00:54:09 -0700

    Adjusted LightSource

--- Audio Visualization Tool/Assets/Scripts/InstantiateCubes.cs
+++ Audio Visualization Tool/Assets/Scripts/InstantiateCubes.cs
@@ -7,16 +7,26 @@ public class InstantiateCubes : MonoBehaviour
     [Range(1000, 100000)]
     [SerializeField] private float _maxScale = 1000f;
 
-    private GameObject[] _sampleCubes = new GameObject[512];
+    private GameObject[] _sampleCubes = null;
 
     private const string CUBE_NAME = "SampleCube";
-    private const float ANGLE_OF_SAMPLE_IN_CIRCLE = 360f / 512f;
+    private float _anglebetweenSamples;
     private const float DISTANCE_BETWEEN_CUBES = 100f;
     private const float CUBE_SCALE = 10f;
+
+    #region TryoutVariables
+
+    [SerializeField] private float _distanceBetweenCubes = 20f;
+    [SerializeField] private float _cubeScale = 5f;
+
+    #endregion
 
 
     private void Start()
     {
+        _sampleCubes = new GameObject[AudioPeer.Samples.Length];
+        _anglebetweenSamples = 360f / AudioPeer.Samples.Length;
+
         for (int i = 0; i < _sampleCubes.Length; i++)
         {
             GameObject instantiatedCube = (GameObject)Instantiate(_sampleCubePrefab);
@@ -24,8 +34,8 @@ public class InstantiateCubes : MonoBehaviour
             instantiatedCube.transform.parent = this.transform;
             instantiatedCube.name = string.Format("{0}{1}", CUBE_NAME, i);
 
-            this.transform.eulerAngles = new Vector3(0, -i * ANGLE_OF_SAMPLE_IN_CIRCLE, 0);
-            instantiatedCube.transform.position = Vector3.forward * DISTANCE_BETWEEN_CUBES;
+            this.transform.eulerAngles = new Vector3(0, -i * _anglebetweenSamples, 0);
+            instantiatedCube.transform.position = Vector3.forward * _distanceBetweenCubes;
 
             _sampleCubes[i] = instantiatedCube;
         }
@@ -37,7 +47,8 @@ public class InstantiateCubes : MonoBehaviour
         {
             if (_sampleCubes[i] != null)
             {
-                _sampleCubes[i].transform.localScale = new Vector3(CUBE_SCALE, AudioPeer.Samples[i] * _maxScale, CUBE_SCALE);
+                //_sampleCubes[i].transform.localScale = new Vector3(_cubeScale, AudioPeer.Samples[i] * _maxScale, _cubeScale);
+                _sampleCubes[i].transform.localScale = new Vector3(AudioPeer.Samples[i] * _maxScale, _cubeScale, _cubeScale);
             }
         }
     }
